2b89f0ef9ded6a9efde5e19a8db52e54c86b2d90
Dylan Timmons <dylanstimmons@icloud.com>
2023-06-13 11:46:13 -0600

    add working most popular ride method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -9,7 +9,11 @@ class Carnival
         @rides = []
     end
 
-    def add_rides(ride)
+    def add_ride(ride)
         @rides << ride
+    end
+
+    def most_popular_ride
+        @rides.max_by { |ride| ride.rider_log.values.sum }
     end
 end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -20,31 +20,33 @@ RSpec.describe Carnival do
         carnival1 = Carnival.new(14)
         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
 
-        expect(carnival1.add_rides(ride1)).to eq([ride1])
+        expect(carnival1.add_ride(ride1)).to eq([ride1])
     end
 
-    it "can generate most popular ride"
-        carnival2 = Carnival.new(12)
-
+    it "returns the most popular ride" do
+        carnival = Carnival.new(12)
+  
         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
-
+  
         visitor1 = Visitor.new('Bruce', 54, '$10')
         visitor2 = Visitor.new('Tucker', 36, '$5')
         visitor3 = Visitor.new('Penny', 64, '$15')
-
+  
         visitor1.add_preference(:gentle)
         visitor2.add_preference(:gentle)
         visitor3.add_preference(:gentle)
-
+  
         ride1.board_rider(visitor1)
         ride1.board_rider(visitor2)
         ride1.board_rider(visitor3)
         ride2.board_rider(visitor1)
         ride2.board_rider(visitor2)
-
-        expect(carnival2.most_popular_ride).to eq(ride2)
-    end
-
+  
+        carnival.add_ride(ride1)
+        carnival.add_ride(ride2)
+  
+        expect(carnival.most_popular_ride).to eq(ride1)
+      end
 
 end
