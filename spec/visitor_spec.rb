af8ec5899b104d7c9737e411b33de1366c847263
Dylan Timmons <dylanstimmons@icloud.com>
2023-06-13 09:16:44 -0600

    set up test for has attributes

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,22 @@
+require './lib/visitor'
+
+RSpec.describe Visitor do
+    describe "#initilize" do
+
+        it "has a name" do
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor1).to be_an(Visitor)
+        end
+
+        it "has attributes" do
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor1.name).to eq("Bruce")
+            expect(visitor1.height).to eq(54)
+            expect(visitor1.spending_money).to eq(10)
+            expect(visitor1.preferences).to eq([])
+        end
+    end
+
+end
