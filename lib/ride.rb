888b095e7cb7cd6dadd058305e2f31270f86ff2b
Dylan Timmons <dylanstimmons@icloud.com>
2023-06-13 11:08:03 -0600

    add test for iteration 2 and add final methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,18 +1,39 @@
 require './lib/visitor'
 
 class Ride
-    attr_reader :name,
-                :min_height,
-                :admission_fee,
-                :excitement,
-                :rider_log
-
+    attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log
+  
     def initialize(attributes)
-        @name = attributes[:name]
-        @min_height = attributes[:min_height]
-        @admission_fee = attributes[:admission_fee]
-        @excitement = attributes[:excitement]
-        @rider_log = {}
+      @name = attributes[:name]
+      @min_height = attributes[:min_height]
+      @admission_fee = attributes[:admission_fee]
+      @excitement = attributes[:excitement]
+      @rider_log = {}
     end
-
-end
+  
+    def total_revenue
+      @rider_log.values.sum * @admission_fee
+    end
+  
+    def board_rider(visitor)
+      return unless can_board?(visitor)
+  
+      deduct_admission_fee(visitor)
+      update_rider_log(visitor)
+    end
+  
+    private
+  
+    def can_board?(visitor)
+      visitor.tall_enough?(@min_height) && visitor.preferences.include?(@excitement)
+    end
+  
+    def deduct_admission_fee(visitor)
+      visitor.spending_money -= @admission_fee
+    end
+  
+    def update_rider_log(visitor)
+      @rider_log[visitor] ||= 0
+      @rider_log[visitor] += 1
+    end
+  end
