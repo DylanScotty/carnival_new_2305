7773349efe756c0638f776391adb4195f041cfb6
Dylan Timmons <dylanstimmons@icloud.com>
2023-06-13 10:10:51 -0600

    Add Ride class with test and initialize

---
+++ lib/ride.rb
@@ -0,0 +1,18 @@
+require './lib/visitor'
+
+class Ride
+    attr_reader :name,
+                :min_height,
+                :admission_fee,
+                :excitement,
+                :rider_log
+
+    def initialize(attributes)
+        @name = attributes[:name]
+        @min_height = attributes[:min_height]
+        @admission_fee = attributes[:admission_fee]
+        @excitement = attributes[:excitement]
+        @rider_log = {}
+    end
+
+end
